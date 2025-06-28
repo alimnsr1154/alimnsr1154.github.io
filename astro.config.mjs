7c5e44c48ac9574b7e298cba575be86dd30002f1
Ali Mansoor <alimansoor4493@gmail.com>
2025-06-29 02:49:00 +0500

    Initial commit from friend's portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thewasif.github.io",
+  site: "https://alimnsr1154.github.io",
 });
